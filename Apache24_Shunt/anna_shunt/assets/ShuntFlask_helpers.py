f7ac0683f34fe7e1c39e28251a1cca5441166466
Kelvin Kong <cykkong@ucdavis.edu>
2024-09-26 11:22:35 -0700

    Upload ShuntFlask_helpers.py

---
+++ Apache24_Shunt/anna_shunt/assets/ShuntFlask_helpers.py
@@ -0,0 +1,6 @@
+log_path = "C:/Apache24_Shunt/logs/anna_Shunt.log"
+
+shunt_main_folder = "C:/Apache24_Shunt/anna_shunt"
+
+pss_model_path = shunt_main_folder + "/assets/pss_classification/trained_models/pss_model.pkl"
+subcat_model_path = shunt_main_folder + "/assets/pss_classification/trained_models/subcat_model.pkl"
